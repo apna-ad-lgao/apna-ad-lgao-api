83deebc7349b717974165d9a31b8d604c1149bc9
Aditya Kumar <adityaverma.dev@gmail.com>
2020-03-28 05:01:40 +0530

    UserCompany added

---
+++ modals/userCompany/userCompany.js
@@ -0,0 +1,42 @@
+module.exports = (sequelize, DataTypes) => {
+    const UserCompany = sequelize.define('UserCompany', {
+      userId: {
+        type: DataTypes.NUMBER,
+        references: {
+          model: 'User',
+          key: 'id',
+        },
+        allowNull: false,
+      },
+      companyId: {
+        type: DataTypes.NUMBER,
+        references: {
+          model: 'Company',
+          key: 'id',
+        },
+        allowNull: false,
+      },
+      isOwner: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+      },
+      isMarketer: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+      },
+      isHidden: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+      },
+      created: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      updated: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    }, {});
+    return UserCompany;
+  };
+  
